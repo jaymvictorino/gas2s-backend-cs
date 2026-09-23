1a7c98f3aa8f95cf10bb4efdc1bc45e4ec4d78e3
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-09-23 14:10:13 +0800

    feat(Handlers): add `GetByIdAsync` method to handle GET requests that has `id` path variable

---
+++ src/Modules/Expenses/Application/Handlers/GetByIdExpenseHandler.cs
@@ -0,0 +1,13 @@
+using Expenses.Application.Dto;
+using Expenses.Application.Interfaces;
+
+namespace Expenses.Application.Handlers;
+
+public class GetByIdExpenseHandler
+{
+    private readonly IExpenseRepository _rep;
+
+    public GetByIdExpenseHandler(IExpenseRepository rep) => _rep = rep;
+
+    public async Task<ExpenseResponse?> GetByIdAsync(Guid id) => await _rep.GetByIdAsync(id);
+}
