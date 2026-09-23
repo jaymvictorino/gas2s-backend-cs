c26964bbf34796365645bfffa79c2830e24ee6bc
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-09-23 14:05:45 +0800

    feat(Handlers): add `CreateExpense` method to handle POST requests

---
+++ src/Modules/Expenses/Application/Handlers/CreateExpenseHandler.cs
@@ -0,0 +1,14 @@
+using Expenses.Application.Dto;
+using Expenses.Application.Interfaces;
+
+namespace Expenses.Application.Handlers;
+
+public class CreateExpenseHandler
+{
+    private readonly IExpenseRepository _rep;
+
+    public CreateExpenseHandler(IExpenseRepository rep) => _rep = rep;
+
+    public async Task<ExpenseResponse> CreateExpense(CreateExpenseRequest expReq) =>
+        await _rep.CreateAsync(expReq);
+}
