9788881b0ee2aebee90fe7925905a2e5cd55125a
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-09-26 16:48:06 +0800

    fix(Exp.App/IExpenseRepository): use `Expense` domain entity instead of DTOs

--- src/Modules/Expenses/Application/Interfaces/IExpenseRepository.cs
+++ src/Modules/Expenses/Application/Interfaces/IExpenseRepository.cs
@@ -6,4 +6,5 @@ public interface IExpenseRepository
 {
     Task<IReadOnlyList<Expense>> GetAllAsync(Guid userId);
     Task<Expense?> GetByIdAsync(Guid userId, Guid);
+    Task<Expense> CreateAsync(Expense expense);
 }
