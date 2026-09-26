646e4910dffdeb13ba1d7c42b782efa9411aeb71
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-09-26 16:51:58 +0800

    feat(Exp.App/IExpenseRepository): add `UpdateAsync` and `DeleteAsync` methods

--- src/Modules/Expenses/Application/Interfaces/IExpenseRepository.cs
+++ src/Modules/Expenses/Application/Interfaces/IExpenseRepository.cs
@@ -5,6 +5,8 @@ namespace Expenses.Application.Interfaces;
 public interface IExpenseRepository
 {
     Task<IReadOnlyList<Expense>> GetAllAsync(Guid userId);
-    Task<Expense?> GetByIdAsync(Guid userId, Guid);
+    Task<Expense?> GetByIdAsync(Guid userId, Guid id);
     Task<Expense> CreateAsync(Expense expense);
+    Task<Expense> UpdateAsync(Expense expense);
+    Task<bool> DeleteAsync(Expense expense);
 }
